59d8cb1c57728414913b6b743174adfa59dacc8c
JonasFonM <prothetbrasil@gmail.com>
2020-07-06 21:40:58 -0300

    Revisitando Login

--- view/login/loginView.php
+++ view/login/loginView.php
@@ -12,7 +12,7 @@ $_SESSION['usuario'] = null;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Faça seu Login</title>
 </head>
 
 <body>
@@ -23,7 +23,7 @@ $_SESSION['usuario'] = null;
         $objUsuario =  Model::logar($_POST["login"], $_POST['senha']);
         if (!empty($objUsuario)) {
             $_SESSION['usuario'] = $objUsuario;
-            header("Location: ../cliente/listarClienteView.php");
+            header("Location: ../veiculo/listarVeiculoView.php");
         } else {
             echo "<b style='color:red;'>Login ou Senha errado, tente novamente! </b>";
         }
